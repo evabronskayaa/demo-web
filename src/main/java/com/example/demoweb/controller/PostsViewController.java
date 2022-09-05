b74766223a9f8e20bf47ad4be67e3005441b856c
eva <eva27122002b@gmail.com>
2022-09-05 09:13:34 +0500

    added thymeleaf template

--- src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -1,14 +1,15 @@
 package com.example.demoweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostsViewController {
-    @ResponseBody
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list() {
-        return "Здесь будет главная страница";
+    public String list(Model model) {
+        model.addAttribute("appName", "Сентябрь горит");
+        return "list";
     }
 
     @ResponseBody
